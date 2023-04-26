5b6b892932cd38c770b1fd69e215eaeff95b2de7
petko <p.todorov1015@gmail.com>
2023-04-26 21:33:47 +0300

    done

--- December_21_2022/plants/urls.py
+++ December_21_2022/plants/urls.py
@@ -4,7 +4,7 @@ from plants import views
 
 urlpatterns = [
     path('create', views.create_plant, name='create_plant'),
-    path('details/<int:id>', views.details_plant, name='details_plant'),
+    path('details/<id>', views.details_plant, name='details_plant'),
     path('edit/<int:id>', views.edit_plant, name='edit_plant'),
     path('delete/<int:id>', views.delete_plant, name='delete_plant'),
 ]

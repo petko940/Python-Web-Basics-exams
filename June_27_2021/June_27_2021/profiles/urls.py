8efa1e2e55168b171c86ed1501464ffab3f118e8
petko <p.todorov1015@gmail.com>
2023-06-11 18:17:39 +0300

    updated

--- June_27_2021/June_27_2021/profiles/urls.py
+++ June_27_2021/June_27_2021/profiles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import profile,delete_profile
 
 urlpatterns = [
-    path('profile', profile, name='profile'),
-    path('profile-delete', delete_profile, name='delete_profile'),
+    path('profile/', profile, name='profile'),
+    path('profile-delete/', delete_profile, name='delete_profile'),
 ]
